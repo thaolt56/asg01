360a0c025c458544c32c998a51b8bbf6d6c8d5c2
thaolt56 <langtrongthao@gmail.com>
2014-07-04 12:39:05 -0700

    bt lon so 1

---
+++ Beer.java
@@ -0,0 +1,11 @@
+public class Beer{
+	public static void main(String[] args){
+			System.out.println ("Ho va ten: Lang Trong Thao\tMssv: 11020430\t Lop:k56CB\t uesname gitHub: thaolt56\tGmail: langtrongthao@gmail.com");
+			for( int a=9; a>=1; a--){
+					System.out.print(a+"bottles of beer on the wall, " +a+ "bottles of beer.\n" + 
+									"tale one down, pass it around,\n");
+			}
+			System.out.print("No more bottles of beer on the wall.");
+	}
+		
+}
